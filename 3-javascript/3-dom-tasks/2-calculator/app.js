8ea14529b358ac993ca0fb5c9a5bde433c87c311
HafizAzan <azan09068@gmail.com>
2023-12-09 23:37:02 +0500

    calculator

---
+++ 3-javascript/3-dom-tasks/2-calculator/app.js
@@ -0,0 +1,39 @@
+const selectButton = document.querySelector(".buttons");
+const selectScreen = document.querySelector(".screen");
+const ClearBtn = document.querySelector(".btn-clear");
+const ClearEqual = document.querySelector(".btn-equal");
+const ClearE = document.querySelector(".btn-E");
+
+// console.log(ClearBtn,"ClearBtn");
+
+selectButton.addEventListener("click", function (event){
+    event.preventDefault();
+    console.log(selectButton,"selectButton");
+    const targetCurrent = event.target;
+    console.log(targetCurrent.classList,"targetCurrent");
+    if(targetCurrent.classList.contains("btn")){ 
+        console.info("btn is working!")
+    const attributeSelect =  targetCurrent.getAttribute("data-num");
+    selectScreen.value  += attributeSelect;
+    console.log(selectScreen.value,"attributeSelect");
+    }
+});
+
+ClearBtn.addEventListener('click',function(e){
+    e.preventDefault();
+    selectScreen.value ="0";
+    console.log(ClearBtn,"ClearBtn");
+});
+
+ClearEqual.addEventListener('click',function(e){
+    e.preventDefault();
+    selectScreen.value = eval(selectScreen.value);
+    console.log(ClearEqual,"ClearEqual");
+});
+
+
+ClearE.addEventListener('click',function(e){
+    e.preventDefault();
+    selectScreen.value =" ";
+    console.log(ClearE,"ClearE");
+});
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # MY-WORK
+Use this command whenever you encounter these problems like (over written by merge ) in a git clone.
+git resset -v
+git pull
